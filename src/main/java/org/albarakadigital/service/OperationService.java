56a3300f2c54e162fe87fd6d444322dfc64a7145
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-12-22 16:41:00 +0100

    Add method to get operations by account in OperationService & add custom query method for operations be source accouont in OperationRepository & change runtime exception with entity not found when necessary

--- src/main/java/org/albarakadigital/service/OperationService.java
+++ src/main/java/org/albarakadigital/service/OperationService.java
@@ -2,8 +2,11 @@ package org.albarakadigital.service;
 
 import org.albarakadigital.entity.Operation;
 
+import java.util.List;
+
 public interface OperationService {
     Operation createDeposit(Long accountId, Double amount);
     Operation createWithdrawal(Long accountId, Double amount);
     Operation createTransfer(Long sourceAccountId, String destinationAccountNumber, Double amount);
+    List<Operation> getOperationsByAccount(Long accountId);
 }
